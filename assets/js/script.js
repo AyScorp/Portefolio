9265b57f33e0c7fe6458235fe54f5c7d28984475
AyScorp <aymericcherbonnier@gmail.com>
2025-04-27 11:00:43 +0200

    details

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 let darkMode = false;
 
 document.getElementById('openPdf').addEventListener('click', function() {
-  window.open('https://perso.univ-lemans.fr/~i2301203/Documents/CV.pdf', '_blank');
+  window.open('https://aym-cher-web.onrender.com//Documents/CV.pdf', '_blank');
 });
 
 document.getElementById('openVideo').addEventListener('click', function() {
@@ -16,9 +16,9 @@ function changeDarkMode() {
     document.documentElement.style.setProperty("--background-colorwrapper", "#AEA89D");
     document.documentElement.style.setProperty("--background-color-icon", "#747069");
     document.documentElement.style.setProperty("--box-shadow-color", "rgba(0, 0, 0, 0.3)");
-    document.documentElement.style.setProperty('--signature-image', "url('images/signature.png')");
-    document.documentElement.style.setProperty('--savoir-image', "url(images/SavoirFaireBlanc.png)");
-    document.documentElement.style.setProperty('--pointFort-image', "url('images/pointFortBlanc.png')");
+    document.documentElement.style.setProperty('--signature-image', "url('../images/signature.png')");
+    document.documentElement.style.setProperty('--savoir-image', "url(../images/SavoirFaireBlanc.png)");
+    document.documentElement.style.setProperty('--pointFort-image', "url('../images/pointFortBlanc.png')");
 
     document.getElementById("dark-light-mode").innerHTML = "Dark mode";
   } else {
@@ -29,15 +29,15 @@ function changeDarkMode() {
     document.documentElement.style.setProperty("--background-color-icon", "rgb(100,100,100)");
     document.documentElement.style.setProperty("--box-shadow-color", "rgba(255, 255, 255, 0.4)");
     document.documentElement.style.setProperty("--signature-image", "url(../images/signature.svg)");
-    document.documentElement.style.setProperty("--savoir-image", "url('images/savoirEtreBlack.png')");
-    document.documentElement.style.setProperty("--pointFort-image", "url('images/PointFortBlack.png')");
+    document.documentElement.style.setProperty("--savoir-image", "url('../images/savoirEtreBlack.png')");
+    document.documentElement.style.setProperty("--pointFort-image", "url('../images/PointFortBlack.png')");
 
     document.getElementById("dark-light-mode").innerHTML = "Light mode";
   }
 }
 
 function goToHome(){
-  document.location.href="Portefolio.html";
+  document.location.href="index.html";
 }
 
 const tabs = document.querySelectorAll('.tab');
